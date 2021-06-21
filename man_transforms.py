82409966a72df8a93b786448bcc28566cd46530e
Lhogeshwaran Purushothaman <lhogeshwaran@yahoo.com>
2021-06-21 14:31:44 +1000

    Added transformations for PTC_24 hours file.

--- man_transforms.py
+++ man_transforms.py
@@ -6,3 +6,16 @@ import pandas as pd
 df = pd.read_excel('datalake/Lists.xlsx')
 df.columns = map(str.lower, df.columns)
 df.to_csv('data/lists.csv')
+
+# Convert PTC_24_hours to timeseries format.
+df = pd.read_csv('datalake/PTC_all_24hours.csv')
+df.drop(columns=['date.1', 'year', 'month'], inplace=True)
+df = df.melt(id_vars=df.columns[:6], var_name='hour', value_name='vehicle_count')
+df['vehicle_count'] = df['vehicle_count'].astype(int)
+df['hour'] = df['hour'].str.replace(pat='hour_', repl='')
+df['date'] = df['date'].str.replace(pat='T00:00:00Z', repl='')
+df['date'] = pd.to_datetime(df['date']+' '+df['hour'])
+df.drop(columns='hour', inplace=True)
+df = df.set_index('date')
+df.to_csv('data/ptc_24_hours_timeseries.csv')
+### Once converted, upload file into influx db
