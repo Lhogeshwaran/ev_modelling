c61d53c390ddc1ff46a42144a0a5294c0989da76
Lhogeshwaran Purushothaman <lhogeshwaran@yahoo.com>
2021-06-23 16:03:40 +1000

    Second method for adding plot with two y-axis.

--- eda.py
+++ eda.py
@@ -12,3 +12,21 @@ df = pd.read_csv('data/ptc_24_hours_timeseries.csv', index_col=0, parse_dates=Tr
 # EDA
 df_codes = pd.DataFrame(df.groupby(df.index.year)['code'].agg(['nunique', 'count']))
 df_codes.plot(kind='bar', secondary_y='count', rot=90)
+
+fig, ax1 = plt.subplots()
+
+color = 'tab:red'
+ax1.set_xlabel('year')
+ax1.set_ylabel('volume', color=color)
+ax1.plot(df_codes.index, df_codes['count'], color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = 'tab:blue'
+ax2.set_ylabel('counters', color=color)  # we already handled the x-label with ax1
+ax2.plot(df_codes.index, df_codes['count'], color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
